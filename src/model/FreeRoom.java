3922e968e0f60f000641ceff300c662582b84e6c
Mohamed Mohamed <mohamedmohamed100@gmail.com>
2023-04-18 11:44:52 -0400

    Added the Free Room class as well as added a constructor with all the arguments

---
+++ src/model/FreeRoom.java
@@ -0,0 +1,8 @@
+package model;
+
+public class FreeRoom extends Room{
+
+    public FreeRoom(String roomNumber, Double price, RoomType enumeration) {
+        super(roomNumber, 0.0, enumeration);
+    }
+}

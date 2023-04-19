b88654630abe647a9d2f12917750118ebf112690
Mohamed Mohamed <mohamedmohamed100@gmail.com>
2023-04-19 10:07:55 -0400

    Added CustomerService and its methods including the customer service singleton so that one instance can be uses throughout.

---
+++ src/service/CustomerService.java
@@ -0,0 +1,27 @@
+package service;
+
+import model.customer.Customer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class CustomerService {
+    private final Map<String, Customer> customers = new HashMap<>();
+    private static final CustomerService SINGLETON = new CustomerService();
+
+
+    public CustomerService() {
+    }
+    public static CustomerService getSingleton() {
+        return SINGLETON;
+    }
+    public void addCustomer(String email, String firstName, String lastName){
+        customers.put(email, new Customer(firstName,lastName, email));
+    };
+    public Customer getCustomer(String email) {
+    return customers.get(email);
+    }
+
+
+}

b3c237bc257cb73d9a676136195cca0a80f7fa98
Mohamed Mohamed <mohamedmohamed100@gmail.com>
2023-04-24 18:14:27 -0400

    Created HotelResource class and methods

---
+++ src/api/HotelResource.java
@@ -0,0 +1,61 @@
+package api;
+
+import model.customer.Customer;
+import model.reservation.Reservation;
+import model.room.IRoom;
+import service.CustomerService;
+import service.ReservationService;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+
+public class HotelResource {
+    private static final HotelResource SINGLETON = new HotelResource();
+
+    private final CustomerService customerService = CustomerService.getSingleton();
+    private final ReservationService reservationService = ReservationService.getSingleton();
+
+    public HotelResource() {}
+
+    public static HotelResource getSingleton() {
+        return SINGLETON;
+    }
+    public Customer getCustomer(String email) {
+        return customerService.getCustomer(email);
+    }
+
+    public void createCustomer(String email, String firstName, String lastName) {
+        customerService.addCustomer(email, firstName, lastName);
+    }
+
+    public IRoom getRoom(String roomNumber) {
+        return reservationService.getARoom(roomNumber);
+    }
+
+    public Reservation bookRoom(String customerEmail, IRoom room, Date checkInDate, Date checkOutDate) {
+        return reservationService.reserveARoom(getCustomer(customerEmail), room, checkInDate, checkOutDate);
+    }
+
+    public Collection<Reservation> getCustomersReservations(String customerEmail) {
+        final Customer customer = getCustomer(customerEmail);
+
+        if (customer == null) {
+            return Collections.emptyList();
+        }
+
+        return reservationService.getCustomersReservation(getCustomer(customerEmail));
+    }
+
+    public Collection<IRoom> findARoom(final Date checkIn, final Date checkOut) {
+        return reservationService.findRooms(checkIn, checkOut);
+    }
+
+    public Collection<IRoom> findAlternativeRooms(final Date checkIn, final Date checkOut) {
+        return reservationService.findAltRooms(checkIn, checkOut);
+    }
+
+    public Date addDefaultPlusDays(final Date date) {
+        return reservationService.addDefaultPlusDays(date);
+    }
+}
